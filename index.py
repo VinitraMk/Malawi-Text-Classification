c89856c23b05a30f578925b91b7ede916e11ce08
Vinitra Muralikrishnan <vinitramk@gmail.com>
2021-04-22 09:11:04 +0000

    finalizing model for linear svm

--- index.py
+++ index.py
@@ -42,35 +42,17 @@ plt.xlabel('Category')
 #plt.show()
 print()
 
-#xgb = XGBoost(train_texts, train_data['Label_Id'], test_data['ID'], le)
-#xgb.predict_and_save_csv(test_texts)
 vectorizer = TfidfVectorizer(sublinear_tf = True, norm = 'l2', ngram_range = (1,2), stop_words = stopwords('ny'))
 train_features = vectorizer.fit_transform(train_texts).toarray()
 test_features = vectorizer.transform(test_texts).toarray()
 print('Transformed features shape: ',train_features.shape)
-K = 5
 label_ids = train_data['Label_Id']
-'''
-for label_id, label in sorted(encoded_labels.items()):
-    train_features_chi2 = chi2(train_features, label_ids == label_id)
-    indices = np.argsort(train_features_chi2[0])
-    feature_names = np.array(vectorizer.get_feature_names())[indices]
 
-    unigrams = [v for v in feature_names if len(v.split(' ')) == 1]
-    bigrams = [v for v in feature_names if len(v.split(' ')) == 2]
-    print("# '{}':".format(label))
-    print("\t. Most correlated unigrams:\n\t\t. {}".format('\n\t\t. '.join(unigrams[-K:])))
-    print("\t. Most correlated bigrams:\n\t\t. {}".format('\n\t\t. '.join(bigrams[-K:])))
-'''
-
-K = 50000
+K = 80000
 kbest = SelectKBest(chi2, k = K)
 train_features_best = kbest.fit_transform(train_features, train_data['Label_Id'])
 test_features_best = kbest.transform(test_features)
 print('\nReduced chi2 features: ', train_features_best.shape, test_features_best.shape)
-
-#xgb = XGBoost(train_features_best, train_data['Label_Id'], test_data['ID'], le)
-#xgb.predict_and_save_csv(test_features_best)
 
 linear_model = LinearSVM(train_features_best, train_data['Label_Id'], test_data['ID'], le)
 linear_model.predict_and_save_csv(test_features_best)
