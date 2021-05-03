327a9380176af7b676b8d95c41208a29903d0d64
VinitraMk <vinitramk@gmail.com>
2021-05-03 22:51:46 +0000

    adding index script with arguments for model selection

--- index.py
+++ index.py
@@ -17,6 +17,7 @@ from modules.vectorizer import Vectorizer
 import warnings
 from stopwordsiso import stopwords as stopwords
 import time
+import sys
 
 st = time.time()
 warnings.filterwarnings('ignore')
@@ -52,7 +53,7 @@ reduced_vocabulary = []
 print('Transformed features shape: ',train_features.shape)
 label_ids = train_data['Label_Id']
 
-K = 200 
+K = 100 
 for label_id, label in sorted(encoded_labels.items()):
     train_features_chi2 = chi2(train_features, label_ids == label_id)
     indices = np.argsort(train_features_chi2[0])
@@ -74,12 +75,15 @@ train_features_best = vectorizer.fit_transform(train_texts).toarray()
 test_features_best = vectorizer.transform(test_texts).toarray()
 print('\nReduced vocabulary features: ', train_features_best.shape, test_features_best.shape)
 
-#xgb = XGBoost(train_features_best, train_data['Label_Id'], test_data['ID'], le)
-#xgb.predict_and_save_csv(test_features_best)
-dtree = DecisionTree(train_features_best, train_data['Label_Id'], test_data['ID'], le)
-dtree.predict_and_save_csv(test_features_best)
-#linear_model = LinearSVM(train_features_best, train_data['Label_Id'], test_data['ID'], le)
-#linear_model.predict_and_save_csv(test_features_best)
+if len(sys.argv)==1:
+    print('No Model Selected :-(\n')
+else:
+    if sys.argv[1] == 'dtree':
+        dtree = DecisionTree(train_features_best, train_data['Label_Id'], test_data['ID'], le)
+        dtree.predict_and_save_csv(test_features_best)
+    elif sys.argv[1] == 'xgb':
+        xgb = XGBoost(train_features_best, train_data['Label_Id'], test_data['ID'], le)
+        xgb.predict_and_save_csv(test_features_best)
 
 et = time.time()
 print('\nMinutes elapsed:',(et - st) * 60 / 3600,'\n')
